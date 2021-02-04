a9310be10a31488fa584ee0e535ad5c01889d2af
RUnroe <ryanunroe@gmail.com>
2021-02-03 17:39:54 -0700

    Add DAL

---
+++ VideoGameLibrary/Interfaces/IDataAccessLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoGameLibrary.Models;
+
+namespace VideoGameLibrary.Interfaces
+{
+    public interface IDataAccessLayer
+    {
+        IEnumerable<VideoGame> GetCollection();
+        IEnumerable<VideoGame> SearchForGames(string key);
+        IEnumerable<VideoGame> FilterCollection(string genre = null, string platform = null, string esrbRating = null);
+        Boolean AddGame(VideoGame game);
+        Boolean DeleteGame(int index);
+
+    }
+}

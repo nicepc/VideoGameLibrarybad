6627c1f5919e39912d304611901fcdc19fd2e569
RUnroe <ryanunroe@gmail.com>
2021-02-26 16:59:54 -0700

    Removed unnecessary files

---
+++ VideoGameLibrary/Migrations/Identity/20210226200438_addUsername.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoGameLibrary.Migrations.Identity
+{
+    public partial class addUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
